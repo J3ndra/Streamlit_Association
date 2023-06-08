93138996782e474c2c93f993b4b20a7404bb5bb9
Endra <endra.kartika.25@gmail.com>
2023-06-09 01:55:55 +0700

    completed code

--- main.py
+++ main.py
@@ -8,93 +8,123 @@ st.title('Aplikasi Web Data Mining Asosiasi!')
 
 st.write("""
          # Dataset
-         ### Grocery History
+         ##### Select dataset!
          """)
 
-data = pd.read_csv('data.csv')
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
 
-st.write(data)
+    st.write(data)
 
-st.write("""
-         # Algorithm
-         ### Apriori
-         """)
+    st.write(f"##### Data shape {data.shape}")
 
-st.write(f"### Data shape {data.shape}")
+    st.write("# Preprocessing")
+    products_list = [product.split(",") for product in data['products'].values]
+    te = TransactionEncoder()
+    te_data = te.fit_transform(products_list)
+    df_encoded = pd.DataFrame(te_data, columns=te.columns_)
+    st.dataframe(df_encoded)
 
-st.write("# Preprocessing")
-products_list = [product.split(",") for product in data['products'].values]
-te = TransactionEncoder()
-te_data = te.fit_transform(products_list)
-df_encoded = pd.DataFrame(te_data, columns=te.columns_)
-st.dataframe(df_encoded)
+    st.write("##### Sum the data")
+    sums = df_encoded.sum().sort_values(ascending=False)
+    st.write(sums)
 
-st.write("### Sum the data")
-sums = df_encoded.sum().sort_values(ascending=False)
-st.write(sums)
+    # Custom Function
+    def ar_iterations(data, num_iter=1, support_value=0.1, iterationIndex=None):
+        # Next Iterations
+        def ar_calculation(iterationIndex=iterationIndex):
+            # Calculation of support value
+            value = []
+            for i in range(0, len(iterationIndex)):
+                result = data.T.loc[iterationIndex[i]].sum()
+                result = len(
+                    result[result == data.T.loc[iterationIndex[i]].shape[0]]) / data.shape[0]
+                value.append(result)
+            # Bind results
+            result = pd.DataFrame(value, columns=["Support"])
+            result["index"] = [tuple(i) for i in iterationIndex]
+            result['length'] = result['index'].apply(lambda x: len(x))
+            result = result.set_index("index").sort_values(
+                "Support", ascending=False)
+            # Elimination by Support Value
+            result = result[result.Support > support_value]
+            return result
 
+        # First Iteration
+        first = pd.DataFrame(df_encoded.T.sum(
+            axis=1) / df_encoded.shape[0], columns=["Support"]).sort_values("Support", ascending=False)
+        first = first[first.Support > support_value]
+        first["length"] = 1
 
-# Custom Function
-def ar_iterations(data, num_iter=1, support_value=0.1, iterationIndex=None):
-    # Next Iterations
-    def ar_calculation(iterationIndex=iterationIndex):
-        # Calculation of support value
-        value = []
-        for i in range(0, len(iterationIndex)):
-            result = data.T.loc[iterationIndex[i]].sum()
-            result = len(
-                result[result == data.T.loc[iterationIndex[i]].shape[0]]) / data.shape[0]
-            value.append(result)
-        # Bind results
-        result = pd.DataFrame(value, columns=["Support"])
-        result["index"] = [tuple(i) for i in iterationIndex]
-        result['length'] = result['index'].apply(lambda x: len(x))
-        result = result.set_index("index").sort_values(
-            "Support", ascending=False)
-        # Elimination by Support Value
-        result = result[result.Support > support_value]
-        return result
+        if num_iter == 1:
+            res = first.copy()
+        # Second Iteration
+        elif num_iter == 2:
+            second = list(itertools.combinations(first.index, 2))
+            second = [list(i) for i in second]
+            res = ar_calculation(second)
+        # All Iterations > 2
+        else:
+            nth = list(itertools.combinations(
+                set(list(itertools.chain(*iterationIndex))), num_iter))
+            nth = [list(i) for i in nth]
+            res = ar_calculation(nth)
 
-    # First Iteration
-    first = pd.DataFrame(df_encoded.T.sum(
-        axis=1) / df_encoded.shape[0], columns=["Support"]).sort_values("Support", ascending=False)
-    first = first[first.Support > support_value]
-    first["length"] = 1
+        return res
 
-    if num_iter == 1:
-        res = first.copy()
+    st.write("# Iteration")
+    st.write("##### First Iteration")
+    iteration1 = ar_iterations(df_encoded, num_iter=1, support_value=0.1)
+    st.write(iteration1)
 
-    # Second Iteration
-    elif num_iter == 2:
+    st.write("##### Second Iteration")
+    iteration2 = ar_iterations(df_encoded, num_iter=2, support_value=0.1)
+    st.write(iteration2)
 
-        second = list(itertools.combinations(first.index, 2))
-        second = [list(i) for i in second]
-        res = ar_calculation(second)
+    st.write("##### Third Iteration")
+    iteration3 = ar_iterations(df_encoded, num_iter=3, support_value=0.01,
+                               iterationIndex=iteration2.index)
+    st.write(iteration3)
 
-    # All Iterations > 2
-    else:
-        nth = list(itertools.combinations(
-            set(list(itertools.chain(*iterationIndex))), num_iter))
-        nth = [list(i) for i in nth]
-        res = ar_calculation(nth)
+    st.write("##### Fourth Iteration")
+    iteration4 = ar_iterations(df_encoded, num_iter=4, support_value=0.01,
+                               iterationIndex=iteration3.index)
+    st.write(iteration4)
 
-    return res
+    st.write("""
+             # Algorithm
+             ##### Apriori
+             """)
 
+    freq_items = apriori(df_encoded, min_support=0.1,
+                         use_colnames=True, verbose=1)
+    st.write(freq_items.sort_values("support", ascending=False))
 
-st.write("### First Iteration")
-iteration1 = ar_iterations(df_encoded, num_iter=1, support_value=0.1)
-st.write(iteration1)
+    st.write("##### Apriori Top 5")
+    st.dataframe(freq_items.head())
 
-st.write("### Second Iteration")
-iteration2 = ar_iterations(df_encoded, num_iter=2, support_value=0.1)
-st.write(iteration2)
+    st.write("##### Apriori Bottom 5")
+    st.dataframe(freq_items.tail())
 
-st.write("### Third Iteration")
-iteration3 = ar_iterations(df_encoded, num_iter=3, support_value=0.01,
-                           iterationIndex=iteration2.index)
-st.write(iteration3)
+    st.write("# Association Rules & Info")
+    df_ar = association_rules(
+        freq_items, metric="confidence", min_threshold=0.5)
+    st.write(df_ar)
 
-st.write("### Fourth Iteration")
-iteration4 = ar_iterations(df_encoded, num_iter=4, support_value=0.01,
-                           iterationIndex=iteration3.index)
-st.write(iteration4)
+    st.write("##### Explanation")
+    st.write("""
+             1. antecedent support : memberitahu kita kemungkinan produk anteseden saja
+             2. consequent support : memberitahu kita kemungkinan konsekuensi produk saja
+             3. support            : memberitahu kita nilai dari kedua produk (Anteseden dan Konsekuen)
+             4. confidence         : memberitahu kita indikasi seberapa sering aturan tersebut ditemukan benar.
+             """)
+
+    st.write("# Show table by filtering from Streamlit")
+    support_value = st.slider("Select Support Value", 0.0, 1.0, 0.5, 0.05)
+    confidence_value = st.slider(
+        "Select Confidence Value", 0.0, 1.0, 0.5, 0.05)
+    filtered_df = df_ar[(df_ar.support > support_value) &
+                        (df_ar.confidence > confidence_value)]
+    sorted_df = filtered_df.sort_values("confidence", ascending=False)
+    st.write(sorted_df)
